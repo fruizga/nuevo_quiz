35fa5ade6e1b3c173502316b0c970081d7b218d4
fruizga <ruizgallego@gmail.com>
2015-08-03 10:34:58 -0700

    Paso 11 Crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,8 @@ router.get('/quizes/answer',quizController.answer);*/
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',	quizController.new);
+router.post('/quizes/create',	quizController.create);
 
 //Autor
 router.get('/author',function(req,res){
